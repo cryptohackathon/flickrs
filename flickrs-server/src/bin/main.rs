92375e22b72f44fb83f489520040596ed91ed1bf
Tom Godden <tgodden@vub.be>
2021-01-09 12:20:10 +0100

    Return values are now JSON

--- flickrs-server/src/bin/main.rs
+++ flickrs-server/src/bin/main.rs
@@ -3,41 +3,85 @@
 #[macro_use] extern crate rocket_contrib;
 extern crate dotenv;
 use rocket::Data;
+use rocket_contrib::json::Json;
 use flickrs_sqlite::*;
 use std::env;
 use dotenv::dotenv;
 use std::fs;
+use serde::Serialize;
 
+/// The struct needed to connect to the database
 #[database("imagesdb")]
 pub struct ImagesDbConn(diesel::SqliteConnection);
 
-fn get_image_bytes(path: Option<String>) -> Vec<u8> {
-    match path {
-        None => Vec::new(),
-        Some(a) => fs::read(a).unwrap_or(Vec::new()),
-    }
+/// The return value of the /<image_id> GET operation
+#[derive(Serialize)]
+struct GetImageReturnValue {
+    succes: bool,
+    image: Option<Vec<u8>>,
+    error: Option<String>,
 }
 
+/// Fetch the bytes of an image with a given path.
+fn get_image_bytes(path: Option<String>) -> Json<GetImageReturnValue> {
+    let value = match path {
+        None => GetImageReturnValue{succes: false, image: None,
+            error: Some("image path was NULL".to_string())},
+        Some(a) => match fs::read(a) {
+            Ok(v) => GetImageReturnValue{succes: true, image: Some(v), error: None},
+            Err(e) => GetImageReturnValue{succes: false, image: None,
+                error: Some(e.to_string())}
+        }};
+    Json(value)
+}
+
+
+/// GET the image with given id. Returns an empty Vec if the image is not found.
+///
+/// ## API
+/// * Address: root/<image_id>
+/// * Data Needed: None
+/// * Returns: {success: bool, image: Byte vector or Null, error: String or Null}
 #[get("/<image_id>")]
-fn get_image(connection: ImagesDbConn, image_id: i32) -> Vec<u8> {
+fn get_image(connection: ImagesDbConn, image_id: i32) -> Json<GetImageReturnValue> {
     match get_image_row(&*connection, &image_id) {
         Ok(row) => get_image_bytes(row.path),
-        _ => Vec::new(),
+        _ => Json(GetImageReturnValue{
+                  succes: false,
+                  image: None,
+                  error: Some(format!("image with id {} does not exist in the database", image_id))}),
     }
 }
 
+/// The return velue from the /upload POST operation
+#[derive(Serialize)]
+struct UploadReturnValue {
+    succes: bool,
+    id: Option<i32>,
+    error: Option<String>,
+}
+
+/// POST a new image to the database. Returns the image's ID, or -1 if something went wrong.
+///
+/// The name and directory for the new image can be changed in .env.
+///
+/// ## API
+/// * Address: root/upload
+/// * Data Needed: Any data
+/// * Returns: {success: bool, id: int or NULL, error: string or Null}
 #[post("/upload", data="<data>")]
-fn upload(connection: ImagesDbConn, data: Data) -> String {
+fn upload(connection: ImagesDbConn, data: Data) -> Json<UploadReturnValue> {
     let id = allocate_image_row(&*connection);
     let filename = format!("{}{}", env::var("IMAGE_BASE_URL").expect("Image path not set!"), id);
     data.stream_to_file(&filename).expect("Could not read file");
     return match set_image_path(&*connection, &id, &filename) {
-        Ok(_) => format!("Image uploaded: {}", id),
-        Err(e) => format! {"Could not upload: {}", e.to_string()},
+        Ok(_) => Json(UploadReturnValue{succes: true, id: Some(id), error: None}),
+        Err(e) => Json(UploadReturnValue{succes: false, id: None, error: Some(e.to_string())}),
     }
 
 }
 
+///Launch the application
 fn main() {
     dotenv().ok();
     rocket::ignite()
--- flickrs-server/src/lib.rs
+++ flickrs-server/src/lib.rs
@@ -7,12 +7,14 @@ use crate::models::Image;
 extern crate dotenv;
 #[macro_use] extern crate diesel;
 
+// Get the last inserted row
 no_arg_sql_function!(
     last_insert_rowid,
     diesel::sql_types::Integer,
     "Represents the SQL last_insert_row() function"
 );
 
+/// Allocate a row for the image, setting the path to NULL. Returns the id of the image.
 pub fn allocate_image_row<'a>(conn: &SqliteConnection) -> i32 {
     use schema::images;
     diesel::insert_into(images::table)
@@ -23,6 +25,7 @@ pub fn allocate_image_row<'a>(conn: &SqliteConnection) -> i32 {
         .unwrap()
 }
 
+/// Set the path of an image with given id.
 pub fn set_image_path<'a>(conn: &SqliteConnection, image_id: &i32, image_path: &str) -> Result<usize, diesel::result::Error> {
     use self::schema::images::dsl::{images,path};
     diesel::update(images.find(image_id))
@@ -30,6 +33,9 @@ pub fn set_image_path<'a>(conn: &SqliteConnection, image_id: &i32, image_path: &
         .execute(conn)
 }
 
+/// Get an image entry
+///
+/// We use .first because the id is UNIQUE in the database scheme
 pub fn get_image_row(conn: &SqliteConnection, image_id: &i32) -> Result<Image, diesel::result::Error> {
     use self::schema::images::dsl::{id,images};
     images.filter(id.eq(image_id))
