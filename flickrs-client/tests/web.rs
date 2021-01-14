10783ca6a9f9d41219fa1cf5c1123ed8dd339acb
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-14 16:49:38 +0100

    tests/web with Dippe::randomized

--- flickrs-client/tests/web.rs
+++ flickrs-client/tests/web.rs
@@ -12,7 +12,7 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 fn end_to_end_conjunction() {
     let mut rng = rand::thread_rng();
-    let dippe = Dippe::new(&mut rng, 2);
+    let dippe = Dippe::randomized(&mut rng, 2);
 
     let (_alice_pub, _alice_priv) = dippe.generate_key_pair(&mut rng);
     let (_bob_pub, _bob_priv) = dippe.generate_key_pair(&mut rng);
