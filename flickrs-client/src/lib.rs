061184d1a147492a78dc55f5f6558e475c6e5d19
Thibaut Vandervelden <thvdveld@vub.be>
2021-01-14 14:31:20 +0100

    Bindings to seal and open

--- flickrs-client/src/lib.rs
+++ flickrs-client/src/lib.rs
@@ -32,3 +32,44 @@ pub fn end_to_end_conjunction() {
 pub fn get_image_title() -> String {
     "Hello from Rust".into()
 }
+
+#[wasm_bindgen]
+pub fn seal(
+    server_key: &JsValue,
+    bytes: &JsValue,
+    policy: &JsValue,
+    attributes: &JsValue,
+) -> JsValue {
+    let authority: PublicKey = server_key.into_serde().unwrap();
+    let bytes: Vec<u8> = bytes.into_serde().unwrap();
+    let policy: Vec<usize> = policy.into_serde().unwrap();
+    let attributes: usize = attributes.into_serde().unwrap();
+
+    let dippe = Dippe::new(b"flickrs", 2);
+    let mut rng = rand::thread_rng();
+
+    let sealed = hybrid::seal(&mut rng, &dippe, &authority, &bytes, &policy, attributes);
+
+    JsValue::from_serde(&sealed).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn open(
+    upk: &JsValue,
+    av: &JsValue,
+    gid: &JsValue,
+    bytes: &JsValue,
+    attributes: &JsValue,
+) -> JsValue {
+    let dippe = Dippe::new(b"flickrs", 2);
+
+    let av: AttributeVector = av.into_serde().unwrap();
+    let upk: UserPrivateKey = upk.into_serde().unwrap();
+    let gid: Vec<u8> = gid.into_serde().unwrap();
+    let attributes: usize = attributes.into_serde().unwrap();
+    let sealed: Vec<u8> = bytes.into_serde().unwrap();
+
+    let open = hybrid::open(&dippe, attributes, &av, &upk, &gid, &sealed);
+
+    JsValue::from_serde(&open).unwrap()
+}
