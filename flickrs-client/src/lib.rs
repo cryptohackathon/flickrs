776098ea9c57dbf2e405c1ccbba5c10298121d63
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-15 11:39:52 +0100

    Remove warning

--- flickrs-client/src/lib.rs
+++ flickrs-client/src/lib.rs
@@ -22,7 +22,7 @@ pub fn end_to_end_conjunction() {
     let mut rng = rand::thread_rng();
     let dippe = Dippe::randomized(&mut rng, 2);
 
-    let (alice_pub, _alice_priv) = dippe.generate_key_pair(&mut rng);
+    let (_alice_pub, _alice_priv) = dippe.generate_key_pair(&mut rng);
     let (_bob_pub, _bob_priv) = dippe.generate_key_pair(&mut rng);
 
     alert("Alice public key calculated");
